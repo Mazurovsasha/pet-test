0cedb3008b485256da64450f42a453c80a97f0f0
Alex Mazurov <mazurovsasha@gmail.com>
2024-07-29 22:25:12 +0300

    mail clear

--- src/helpers/validateInputString.ts
+++
@@ -1,5 +0,0 @@
-// Checking data to be a string
-export const validateInputString = (str: string): boolean => {
-  const regex = /^[a-zA-Z0-9@._\- !?,;:()а-яА-ЯёЁ]*$/ // Добавил пару символов и кириллицу
-  return typeof str === 'string' && regex.test(str)
-}
--- src/types/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Task {
-  title: string;
-  items: string[];
-}
-
-export interface RoadmapCardProps {
-  quarter: string;
-  tasks: Task[];
-}
